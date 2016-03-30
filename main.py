6f8712624865dc022ae60f2b10b0a0d5f966969f
Josh Trommel <joshtrommel@gmail.com>
2016-03-29 23:43:38 -0700

    Added better 404 support and removed debug mode

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def earthquakes():
 
 @app.errorhandler(404)
 def page_not_found(e):
-	return jsonify(error=404), 404
+	return render_template('404.html'), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
